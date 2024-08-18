61b625f22980fcbe70e0b2f1bd751f7be1f4e0d6
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-18 14:27:57 -0400

    Create grid square in javascript

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let grid = document.querySelector("#grid")
-let gridSq = document.createElement("div")
+let grid = document.querySelector("#grid");
 
+// MAKE THE GRID
+    // Create grid square
+let gridSq = document.createElement("div");
+    //Define grid square style
+gridSq.setAttribute = ("style", "outline: 1px, red, solid;");
+gridSq.style.width = "8px;";
+    // Ask user for grid size 
+    // Place grid squares in flexbox depending on user answer 
+grid.appendChild(gridSq);
+// FILL THE GRID
+    // Create a event listener for hovering over squares 
+    // Use the event listener to change square style 
