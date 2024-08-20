b344bef9c94949926ab21eb7fcf8ba795bd51a16
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-19 22:15:27 -0400

    Clone node code added

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let gridSq = document.createElement("div");
     //Define grid square style
 gridSq.setAttribute("style", "border: 1px, red, solid; width: 100px; height: 100px;");
 grid.appendChild(gridSq);
+let gridSqTwo = gridSq.cloneNode(true);
+grid.appendChild(gridSqTwo);
 
 //
 
