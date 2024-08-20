269e1f10f6e176ba267bcca71733cc3bf41ac763
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-19 23:40:19 -0400

    Use for loop to create a 3 x 3 grid

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ const grid = document.querySelector("#grid");
 
 // MAKE THE GRID
     // Create grid square
-let gridSq = document.createElement("div");
     //Define grid square style
-gridSq.setAttribute("style", "border: 1px, red, solid; width: 100px; height: 100px;");
-grid.appendChild(gridSq);
-let gridSqTwo = gridSq.cloneNode(true);
-grid.appendChild(gridSqTwo);
+
 
 //Create a for loop that creates gridSq clones until the grid is full 
 function createGrid() {
     for (let i = 0; i < 9; i++) {
-        let gridSqTwo = gridSq.cloneNode(true);
-        grid.appendChild(gridSqTwo);
+        let gridSq = document.createElement("div");
+        gridSq.setAttribute("style", "border: 1px, red, solid; width: 100px; height: 100px;");
+        grid.appendChild(gridSq);
     }
 }
+
+createGrid();
 
 // Ask user for grid size 
 // Place grid squares in flexbox depending on user answer 
