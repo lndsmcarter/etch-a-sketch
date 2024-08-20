fda12d27526f3a626f57d42967f8bdae0f957650
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-19 21:09:14 -0400

    Adjust gridSq style

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const grid = document.querySelector("#grid");
     // Create grid square
 let gridSq = document.createElement("div");
     //Define grid square style
-gridSq.setAttribute("style", "border: 1px, red, solid");
+gridSq.setAttribute("style", "border: 1px, red, solid; width: 60px; height: 60px;");
     // Ask user for grid size 
     // Place grid squares in flexbox depending on user answer 
-grid.appendChild(gridSq);
+    grid.appendChild(gridSq);
+    grid.appendChild(gridSq);
+    grid.appendChild(gridSq);
 // FILL THE GRID
     // Create a event listener for hovering over squares 
     // Use the event listener to change square style 
