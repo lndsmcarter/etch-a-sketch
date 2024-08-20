78fe4b7c45966d7d2453eeb1bbd673ed599492f7
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-19 21:16:51 -0400

    Create and adjust flexbox to justify center, auto size

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ const grid = document.querySelector("#grid");
 let gridSq = document.createElement("div");
     //Define grid square style
 gridSq.setAttribute("style", "border: 1px, red, solid; width: 60px; height: 60px;");
+grid.appendChild(gridSq);
     // Ask user for grid size 
     // Place grid squares in flexbox depending on user answer 
-    grid.appendChild(gridSq);
-    grid.appendChild(gridSq);
-    grid.appendChild(gridSq);
-// FILL THE GRID
+
+    // FILL THE GRID
     // Create a event listener for hovering over squares 
     // Use the event listener to change square style 
