3b10c07336fa471f77a4e51afa9f411a4682cea4
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-21 22:32:34 -0400

    Update README file with skills used

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 PROJECT: TOP Foundations 
 
 SKILLS USED: 
+    - querySelectors 
+    - querySelectorAll --> conversion to array 
+    - using input to create content and manipulate the DOM for interactive activity 
+    - determine if a value is valid/NaN
+    - manipulate styles in the DOM 
+    - use event listeners for buttons and hovers 
--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const grid = document.querySelector("#grid");
     let gridSize = 950;
     grid.setAttribute("style", `border: darkgrey, solid; width: ${gridSize}px; height: ${gridSize}px; margin: 50px;`);
 
-//Ask user for grid size (via number of squares on one side) and put into variable gridSize
 let input = document.querySelector("#gridSize")
 let btn = document.querySelector("button")
 let gridDimension
 let gridSq
 
-
-//Create function for button press
 function btnPress() {
     grid.innerHTML = "";
     let inVal = input.value;
@@ -49,17 +46,3 @@ function btnPress() {
 }
 
 btn.addEventListener("click", btnPress);
-
-//Divide length of grid by gridDimension and save into a variable, gridSqLen
-//Use gridSqLen to determine the size of gridSq
-//Use for loop with i< (gridSize * gridSize)
-
-//Create a for loop that creates gridSq's until the grid is full 
-
-
-// Ask user for grid size 
-// Place grid squares in flexbox depending on user answer 
-
-// FILL THE GRID
-// Create a event listener for hovering over squares 
-// Use the event listener to change square style 
